3c568baf6b1ebc59d928e0e96c6c1a8a7ce583c0
Mermas-CC <27junio03@gmail.com>
2023-03-03 19:29:59 -0500

    pendiente lista de productos

--- scipt.js
+++ scipt.js
@@ -42,3 +42,13 @@ function togglemenucito(){
     }
     menu_email.classList.toggle('inactive')
 }
+
+//----------------------------PRODUCTOS-----------------------------------------
+const product= []
+product.push({
+    nombre: 'bike',
+    precio: '100',
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    descripcion : 'la mejor bike del mundo'
+
+})
